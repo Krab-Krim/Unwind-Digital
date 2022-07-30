450c5f653e4cb7b977cd14992b109f375fddbb97
Krab-Krim <rm2zvk@mail.ru>
2022-07-30 18:38:26 +0300

    test3

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router({mergeParams: true});
 
 router.use("/auth", require("./auth.routes"));
-router.use("/comment", require("./comment.routes"));
 router.use("/user", require("./user.routes"));
 
 module.exports = router;
